820c0befc020a2b66d4d547a9a8df6976c24bb77
agarwalriddhi <join.rids@gmail.com>
2020-04-21 21:58:56 +0000

    pick card

--- uno.js
+++ uno.js
@@ -1,24 +1,29 @@
 var color=['Red', 'Green', 'Yellow', 'Blue'];
-var wild = ['+2', '+4', 'Reverse', 'Skip', 'Special'];
+var wild = ['+2',  'Reverse', 'Skip','+4 and pick color', 'pick color'];
 var canCahngeColor=true;
-var COUNTER=1;
+var COUNTER=0;
 
-var colorNumber=0;
-for(;; ){
-    var cardNumber=0;     
-    for(;;){
-        console.log(COUNTER++, ') Card: ', color[colorNumber], cardNumber);
-        cardNumber++;
-        if(cardNumber>9){
-            break;
-        }
-    }
-    // Findout how Loop 18 - 20 is different from loop 8,9-15
-    // Tell which is more confusing;
-    for(var cardNumber=1; cardNumber<=9; cardNumber++){
-        console.log(COUNTER++, ') Card: ', color[colorNumber], cardNumber);
-    }
-    colorNumber++;
-    if(colorNumber>3)
-        break;
+function pickRandom(cards){
+    var num= Math.floor(Math.random()*108);
+    return cards[num];
 }
+
+var cards = [];
+for(var colorNumber=0;colorNumber<4;colorNumber++ ){
+         
+    for(var cardNumber=0;   cardNumber<10;  cardNumber++   ){
+        cards[COUNTER++]=color[colorNumber] + cardNumber;
+    }
+    for(var cardNumber=1; cardNumber<=9; cardNumber++){
+        cards[COUNTER++]=color[colorNumber]+cardNumber;
+    }
+    for(var wildCardNumder=0; wildCardNumder<5; wildCardNumder++){
+        cards[COUNTER++]= color[colorNumber]+wild[wildCardNumder];    
+    }
+    for(var wildCardNumder=0; wildCardNumder<3; wildCardNumder++){
+        cards[COUNTER++]= color[colorNumber]+wild[wildCardNumder];    
+    }
+}
+
+var random_card=pickRandom(cards);
+console.log('Choosen Card:', random_card);
