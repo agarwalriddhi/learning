bff1c371d082df3554f1509d54892eeeb66cac25
agarwalriddhi <join.rids@gmail.com>
2020-04-21 02:38:13 +0000

    read me

--- uno.js
+++ uno.js
@@ -1,29 +1,10 @@
-// card
-//how many
-// type of cards
-// properties of a card
-/*
-Color: Red,Green, Blue, Yellow
-Number: 1,2,3,4,5,6,7,8,9
-WildCards:+2, +4, Skip, Reverse, Choose Color, 
-108
-
-card= {
-    number:1,
-    color: blue,
-    isWilid: false,
-    typeOfWild:None,
-    action:
-}
-*/
-var cardNumber=1;
 var color=['Red', 'Green', 'Yellow', 'Blue'];
 var wild = ['+2', '+4', 'Reverse', 'Skip', 'Special'];
 var canCahngeColor=true;
-var colorNumber=0;
 var COUNTER=1;
 
-for(;;){
+var colorNumber=0;
+for(;; ){
     var cardNumber=0;     
     for(;;){
         console.log(COUNTER++, ') Card: ', color[colorNumber], cardNumber);
@@ -32,8 +13,9 @@ for(;;){
             break;
         }
     }
-    var cardNumber=1;     
-    for(var cardNumber=1;cardNumber<10;cardNumber++){
+    // Findout how Loop 18 - 20 is different from loop 8,9-15
+    // Tell which is more confusing;
+    for(var cardNumber=1; cardNumber<=9; cardNumber++){
         console.log(COUNTER++, ') Card: ', color[colorNumber], cardNumber);
     }
     colorNumber++;
