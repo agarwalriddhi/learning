991fd938d24ca62f8291d2e9bd2d38571435e654
agarwalriddhi <join.rids@gmail.com>
2020-04-21 01:35:56 +0000

    learn updaown loop

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 console.log('Riddhi Agarwal')
+
+var a = 9
+for (;;){
+  var number=10;
+  var calculated=0;
+  calculated = number -a ;
+  if(a<5)
+    calculated=a;
+  console.log ('This is number ' , calculated)
+  a--
+  if (a<1){
+    break;
+  }
+}
